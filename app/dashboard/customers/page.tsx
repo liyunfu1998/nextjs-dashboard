efc006316a377bf5b9dc874ad07ecad1a8a70aa3
wb_liyunfu <wb_liyunfu@meituan.com>
2024-03-29 16:52:15 +0800

    feat: 添加metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default  function Page (){
-    return <p>Customers Page </p>
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+export default function Page() {
+  return <p>Customers Page </p>;
 }
